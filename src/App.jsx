57074711905d23c4779e3d3c0c858923cafde98a
AMASU <ammarmassoud46@gmail.com>
2025-08-20 01:13:19 +0300

    remove # link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import {  Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -20,15 +20,15 @@ function App() {
    <Router>
        <Navbar/>
        <Routes>
-        <Route path="#/" element={<Home />} />
-        <Route path="#/about" element={<About />} />
-        <Route path="#/contact" element={<Contact />} />
-         <Route path="#/gallery" element={<Gallery />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+         <Route path="/gallery" element={<Gallery />} />
           <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/posts/create" element={<CreatePost />} />
         <Route path="/admin/posts/edit/:id" element={<EditPost />} />
-         <Route path="#/news" element={<News />} />
+         <Route path="/news" element={<News />} />
          <Route path="/news/:id" element={<PostDetail />} />
         
   
